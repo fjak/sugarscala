df8f7cdc20ced3318303beb6b73a61e6f81de3ed
Florian Jakob <github@fjak.de>
2013-12-04 18:16:16 +0100

    Make pull-sugar-libs.sh more robust

--- pull-sugar-libs.sh
+++ pull-sugar-libs.sh
@@ -4,6 +4,7 @@
 # dependencies manually
 
 base=$(dirname "$0")
+mkdir -p "$base/lib/extra"
 cd "$base/lib/extra"
 curl -LO http://files.fjak.de/Scala.tbl
 curl -LO http://update.sugarj.org/plugins/org.spoofax.jsglr_1.2.0.201309171843.jar
