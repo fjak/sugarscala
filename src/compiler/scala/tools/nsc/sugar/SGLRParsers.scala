ff3bfb7c7154c8a6d00cc14ae12fd5221e29a6ce
Florian Jakob <github@fjak.de>
2013-12-04 16:01:54 +0100

    Create scalac AST for simplest object

--- src/compiler/scala/tools/nsc/sugar/SGLRParsers.scala
+++ src/compiler/scala/tools/nsc/sugar/SGLRParsers.scala
@@ -13,6 +13,7 @@ import java.io.FileReader
 trait SGLRParsers {
   val global : Global
   import global._
+  import treeBuilder.{global => _, _}
 
   // TODO: figure out better way to get the parse table file
   val scala_tbl_file = new File("../../../lib/Scala.tbl")
@@ -20,7 +21,7 @@ trait SGLRParsers {
   val parser = SGLRParser
 
   def toScalacAST(term: Term): Tree = {
-    PackageDef(Ident("foo"), List())
+    hello_world
   }
 
   class SGLRUnitParser(unit: global.CompilationUnit) {
@@ -30,6 +31,34 @@ trait SGLRParsers {
       toScalacAST(wrapped_term)
     }
   }
+
+  def superCall =
+    Apply(
+      gen.mkSuperSelect,
+      Nil)
+
+  def defaultInit =
+    DefDef(
+      NoMods,
+      nme.CONSTRUCTOR,
+      Nil,
+      ListOfNil,
+      TypeTree(),
+      Block(
+        List(superCall),
+        Literal(Constant())))
+
+  def hello_world =
+    PackageDef(
+      Ident(nme.EMPTY_PACKAGE_NAME),
+      List(
+        ModuleDef(
+          Modifiers(),
+          "HelloWorld",
+          Template(
+            List(scalaAnyRefConstr),
+            emptyValDef,
+            List(defaultInit)))))
 
   object SGLRParser {
     val tb = new TreeBuilder
