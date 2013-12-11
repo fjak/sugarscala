cc99056cf38d8bda0790e054280ca3fe4cc3fbd3
Florian Jakob <github@fjak.de>
2013-12-11 18:52:59 +0100

    Add some basic tests

---
+++ src/eclipse/scala-compiler/test/hello/HelloWorldDef.scala
@@ -0,0 +1,7 @@
+package hello
+
+object HelloWorldDef {
+  def main(args: Array[String]) = {
+    println("Hello World!")
+  }
+}
---
+++ src/eclipse/scala-compiler/test/hello/HelloWorldProc.scala
@@ -0,0 +1,7 @@
+package hello
+
+object HelloWorldProc {
+  def main(args: Array[String]) {
+    println("Hello World!")
+  }
+}
