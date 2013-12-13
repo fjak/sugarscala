5492d62ff1e5950e0686bb5573aaa4ca01d3d065
Florian Jakob <github@fjak.de>
2013-12-13 15:40:22 +0100

    Add treehugger library

--- pull-sugar-libs.sh
+++ pull-sugar-libs.sh
@@ -9,4 +9,5 @@ cd "$base/lib/extra"
 curl -LO http://files.fjak.de/scala-sugar-20131211.jar
 curl -LO http://update.sugarj.org/plugins/org.spoofax.jsglr_1.2.0.201309171843.jar
 curl -LO http://update.sugarj.org/plugins/org.spoofax.terms_1.2.0.201309171843.jar
+curl -LO http://search.maven.org/remotecontent?filepath=com/eed3si9n/treehugger_2.10/0.3.0/treehugger_2.10-0.3.0.jar
 sha1sum -c ../../sugar-libs.sha1sum
