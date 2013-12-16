9f14207c3a08d8a1ef4780bb2cac6bbbd8938149
Florian Jakob <github@fjak.de>
2013-12-16 12:31:23 +0100

    Add enough code to allow parsing of HelloObject

--- src/compiler/scala/tools/nsc/sugar/SGLRParsers.scala
+++ src/compiler/scala/tools/nsc/sugar/SGLRParsers.scala
@@ -21,17 +21,62 @@ trait SGLRParsers {
   val scala_tbl = ParseTableManager.loadFromStream(scala_tbl_stream)
   val parser = SGLRParser
 
-  def toScalacAST(term: Term): Tree = {
-    hello_world
+  case class IObjectDef(mods: Modifiers, name: TermName) extends Tree
+
+  def toIScalacAST(term: Term): Tree = term match {
+    case "CompilationUnit" @@ (Lst(pkgs@_*), Lst(topStats@_*)) => toPackageDef(pkgs, topStats)
+    case "TopStatSemi" @@ (topStat, _) => toIScalacAST(topStat)
+    case "TopTmplDef" @@ (mods, annots, "Object" @@ ("ObjectDef" @@ (name, body))) =>
+      toModuleDef(mods, annots, name, body)
+    case _ => sys.error(s"Can not translate term ${term} to scalac AST")
+  }
+
+  def toPackageDef(pkgs: Seq[Term], topStats: Seq[Term]) = pkgs match {
+    case Nil => PackageDef(Ident(nme.EMPTY_PACKAGE_NAME), (topStats map toIScalacAST).toList)
+    case _ => sys.error(s"Can not translate ${pkgs} for PackageDef")
+  }
+
+  def toMods(mods: Term, annots: Term) = mods match {
+    case @@("None") => Modifiers()
+    case _ => sys.error(s"Can not translate ${mods} to Modifiers")
+  }
+
+  def toModuleDef(mods: Term, annots: Term, name: Term, body: Term) = body match {
+    case @@("EmptyClassTemplateOpt") => IObjectDef(toMods(mods, annots), toTermName(name))
+    case _ => sys.error(s"Can not translate ${body} for ModuleDef")
+  }
+
+  def toTermName(name: Term) = name match {
+    case "Id" @@ Str(name) => name
+    case _ => sys.error(s"Can not translate ${name} to TermName")
+  }
+
+  def toTemplate(body: Term) = body match {
+    case @@("EmptyClassTemplateOpt") =>
+  }
+
+  object ToFullScalacASTTransformer extends Transformer {
+    override def transform(tree: Tree): Tree = tree match {
+      case IObjectDef(mods, name) => ModuleDef(mods, name, defaultTemplate)
+      case _ => super.transform(tree)
+    }
   }
 
   class SGLRUnitParser(unit: global.CompilationUnit) {
     def parse(): Tree = {
       val stratego_term = parser.parse(unit.source)
       val wrapped_term = Term(stratego_term)
-      toScalacAST(wrapped_term)
+      val iScalacAST = toIScalacAST(wrapped_term)
+      val fullyTransformed = ToFullScalacASTTransformer.transform(iScalacAST)
+      fullyTransformed
     }
   }
+
+  def defaultTemplate =
+    Template(
+      List(scalaAnyRefConstr),
+      emptyValDef,
+      List(defaultInit))
 
   def superCall =
     Apply(
@@ -49,17 +94,17 @@ trait SGLRParsers {
         List(superCall),
         Literal(Constant())))
 
-  def hello_world =
-    PackageDef(
-      Ident(nme.EMPTY_PACKAGE_NAME),
-      List(
-        ModuleDef(
-          Modifiers(),
-          "HelloWorld",
-          Template(
-            List(scalaAnyRefConstr),
-            emptyValDef,
-            List(defaultInit)))))
+  //def hello_world =
+  //  PackageDef(
+  //    Ident(nme.EMPTY_PACKAGE_NAME),
+  //    List(
+  //      ModuleDef(
+  //        Modifiers(),
+  //        "HelloWorld",
+  //        Template(
+  //          List(scalaAnyRefConstr),
+  //          emptyValDef,
+  //          List(defaultInit)))))
 
   object SGLRParser {
     val tb = new TreeBuilder
@@ -80,10 +125,9 @@ trait SGLRParsers {
     def loadFromFile(file: File) = ptm.loadFromStream(new FileInputStream(file))
   }
 
+  abstract class Term
 
-  trait Term
-
-  case class Appl(name: String, children: Term*) extends Term {
+  case class @@(name: String, children: Term*) extends Term {
     override def toString = {
       val t = children map {_.toString}
       s"${name}(${t.mkString(", ")})"
@@ -104,7 +148,7 @@ trait SGLRParsers {
   object Term {
     def apply(term: IStrategoTerm): Term = term match {
       case appl: StrategoAppl =>
-        Appl(appl.getName, appl.getAllSubterms() map {Term(_)}: _*)
+        @@(appl.getName, appl.getAllSubterms() map {Term(_)}: _*)
       case lst: StrategoList =>
         Lst(lst.getAllSubterms() map {Term(_)}: _*)
       case str: StrategoString =>
