d0bfe6b73d791712ad379e85eee00ed1b14e0e71
Florian Jakob <github@fjak.de>
2014-01-07 12:27:23 +0100

    Update Scala SGLR parse table to newest version

--- pull-sugar-libs.sh
+++ pull-sugar-libs.sh
@@ -6,7 +6,7 @@
 base=$(dirname "$0")
 mkdir -p "$base/lib/extra"
 cd "$base/lib/extra"
-curl -LO http://files.fjak.de/scala-sugar-20140103.jar
+curl -LO http://files.fjak.de/sugarscala-v0.3.1.jar
 curl -LO http://update.sugarj.org/plugins/org.spoofax.jsglr_1.2.0.201309171843.jar
 curl -LO http://update.sugarj.org/plugins/org.spoofax.terms_1.2.0.201309171843.jar
 curl -LO http://search.maven.org/remotecontent?filepath=commons-lang/commons-lang/2.6/commons-lang-2.6.jar
